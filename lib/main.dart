78ddc596a98ae6810fcebbf6314011fda83510db
rensawamo <ren0112ngzk@gmail.com>
2024-02-19 18:03:37 +0900

    証明が通らない

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter3',
+      title: 'Flutte4',
       theme: ThemeData(
         // This is the theme of your application.
         //
