c6ca3045725267c72fbc9959f2acd07c4fc6054d
rensawamo <ren0112ngzk@gmail.com>
2024-02-20 18:33:42 +0900

    ios destributionを開始していなかった

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutte6',
+      title: 'Flutte7',
       theme: ThemeData(
         // This is the theme of your application.
         //
