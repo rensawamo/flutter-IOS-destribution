191095c7f857334372d6a3a6f7f05ee61d87824f
rensawamo <ren0112ngzk@gmail.com>
2024-02-20 18:23:15 +0900

    環境変数変更

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutte５',
+      title: 'Flutte6',
       theme: ThemeData(
         // This is the theme of your application.
         //
